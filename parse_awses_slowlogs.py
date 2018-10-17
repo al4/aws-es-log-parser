553f2d3f69deef12365fd12ef697f1d7c1f73902
Alex Forbes <alex.forbes@starlingbank.com>
2018-10-17 15:59:14 +0100

    wip

--- parse_awses_slowlogs.py
+++ parse_awses_slowlogs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import json
 import sys
@@ -6,16 +7,18 @@ import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
-UNTERMINATED = "Unterminated string"
-EXPECTING_OBJECT = "Expecting object"
 EXPECTING_COLON = "Expecting : delimiter"
-EXPECTING_PROPERTY = "Expecting property name:"
-OOB = "end is out of bounds"
-EXPECTING_COMMA = "Expecting , delimiter"
+EXPECTING_COMMA = "Expecting ',' delimiter"
+EXPECTING_OBJECT = "Expecting object"
+EXPECTING_PROPERTY = "Expecting property name"
+EXPECTING_VALUE = "Expecting value:"
 NO_JSON = "No JSON object could be decoded"
+OOB = "end is out of bounds"
+UNTERMINATED = "Unterminated string"
 
 
 def parse_truncated_json(s, depth=0, last_error=None):
+    log.debug('---')
     log.debug(s)
     if depth > 10:
         raise Exception("Too deep for string {}, last error: {}".format(
@@ -23,10 +26,9 @@ def parse_truncated_json(s, depth=0, last_error=None):
     try:
         o = json.loads(s)
     except ValueError as e:
-        log.debug('string {} gives {}'.format(s, e.args[0]))
         err = parse_error(e.args[0])
-        log.debug('err {}: {} (last: {})'.format(err, e.args[0], last_error))
-
+        log.debug('string: `{}`; err `{}`; last: `{}`)'.format(
+            s, err, last_error))
         depth += 1
         if err == UNTERMINATED:
             if last_error == err:
@@ -34,6 +36,7 @@ def parse_truncated_json(s, depth=0, last_error=None):
             else:
                 s = s + '"'
         elif find_open_bracket(s):
+            log.debug('FOOO')
             c = find_open_bracket(s)
             if c:
                 s = s + c
@@ -53,9 +56,11 @@ def parse_truncated_json(s, depth=0, last_error=None):
         elif err == OOB and s[-1:] == ':':
             s = s + '""'
         elif err is EXPECTING_COMMA and last_error is EXPECTING_COMMA:
-            s = s[:-5]
+            s = s[:-4]
         elif err is EXPECTING_COMMA:
             s = s + ','
+        elif err is EXPECTING_VALUE:
+            s = s + ""
         elif err == NO_JSON:
             s = s[:-1]
         else:
@@ -66,23 +71,34 @@ def parse_truncated_json(s, depth=0, last_error=None):
 
 
 def find_open_bracket(s):
+    log.debug("find: {}".format(s))
     lsb = s.rfind('[')
     rsb = s.rfind(']')
-    if lsb and (not rsb or lsb > rsb):
+    if lsb > -1 and (rsb == -1 or lsb > rsb):
         sb_append = ']'
     else:
         sb_append = None
+    log.debug('sb_append: {}'.format(sb_append))
 
     lbr = s.rfind('{')
     rbr = s.rfind('}')
-    if lbr and (not rbr or lbr > rbr):
+    log.debug('lbr: {}, rbr {}'.format(lbr, rbr))
+    if lbr > -1 and (rbr == -1 or lbr > rbr):
         br_append = '}'
     else:
         br_append = None
+    log.debug('br_append: {}'.format(br_append))
 
-    if lbr > lsb:
+    log.debug('lbr: {}, lsb: {}'.format(lbr, lsb))
+    if sb_append and not br_append:
+        return sb_append
+    elif br_append and not sb_append:
+        return br_append
+    elif lbr > lsb:
+        log.debug('br')
         return br_append
     else:
+        log.debug('sb')
         return sb_append
 
 
@@ -101,6 +117,8 @@ def parse_error(msg):
         return EXPECTING_COMMA
     if NO_JSON in msg:
         return NO_JSON
+    if EXPECTING_VALUE in msg:
+        return EXPECTING_VALUE
     return None
 
 
@@ -115,28 +133,47 @@ def find_field(s, name='source'):
     except ValueError:
         return None
 
-    return line[x:y]
+    return s[x:y]
+
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
+
+def find_level(s):
+    x = find_nth(s, '[', 2) + 1
+    try:
+        y = s.index(']', x)
+    except ValueError:
+        return None
+    return s[x:y]
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument('--log-level')
-    args = p.parse_args() 
+    p.add_argument('out_file')
+    args = p.parse_args()
     _l = args.log_level or 'info'
     level = getattr(logging, _l.upper())
     log.setLevel(level)
+
+    f = open(args.out_file, 'w')
 
     failed = 0
     succeeded = 0
     for line in sys.stdin.readlines():
         out = {}
-        source = find_field(line, name='source')
-        log.debug("Source: {}".format(source))
-        took = find_field(line, name='took')
-        out['took'] = took
-        log.debug("Took: {}".format(took))
+        out['took'] = find_field(line, name='took')
+        out['level'] = find_level(line)
+        log.debug(out)
 
-        if not all((source, took)):
+        source = find_field(line, name='source')
+        if not all((source, out['took'])):
             log.info("Skipping message '{}'".format(line))
             continue
 
@@ -147,10 +184,14 @@ if __name__ == "__main__":
             failed += 1
             log.warn("Failed to parse line; Error: {}; line: {}".format(
                 e.args[0], line))
+            print(line)
             continue
         out['source'] = o
         succeeded += 1
-        log.info(out)
+
+        f.write(json.dumps(out))
+        # log.info(out)
+    f.close()
 
     log.info("S: {}; F: {}".format(succeeded, failed))
-    # {"journald_message":"23:34:12.641Z\\",\\"cardUid\\":\\"ed5e704a-eed5-406f-b307-e7782ac54951\\",\\"billingA"}
+
