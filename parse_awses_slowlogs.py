94f8ffbb653076a84e5b6f3df544e4bb2459fbed
Alex Forbes <alex.forbes@starlingbank.com>
2018-10-17 18:32:23 +0100

    INFO:root:S: 14546; F: 365

--- parse_awses_slowlogs.py
+++ parse_awses_slowlogs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import re
 import sys
 import logging
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
-EXPECTING_COLON = "Expecting : delimiter"
+EXPECTING_COLON = "Expecting ':' delimiter"
 EXPECTING_COMMA = "Expecting ',' delimiter"
 EXPECTING_OBJECT = "Expecting object"
 EXPECTING_PROPERTY = "Expecting property name"
@@ -18,45 +19,50 @@ UNTERMINATED = "Unterminated string"
 
 
 def parse_truncated_json(s, depth=0, last_error=None):
-    log.debug('---')
-    log.debug(s)
     if depth > 10:
         raise Exception("Too deep for string {}, last error: {}".format(
             s, last_error))
     try:
         o = json.loads(s)
-    except ValueError as e:
-        err = parse_error(e.args[0])
-        log.debug('string: `{}`; err `{}`; last: `{}`)'.format(
-            s, err, last_error))
+    except (ValueError, json.decoder.JSONDecodeError) as e:
+        err, col = parse_error(e.args[0])
+        log.debug('string: `{}`; err `{}`; last: `{}`'.format(
+            s, e.args[0], last_error))
         depth += 1
-        if err == UNTERMINATED:
-            if last_error == err:
-                s = s + '}'
-            else:
-                s = s + '"'
+
+        log.debug("len: {}, col: {}".format(len(s), col))
+        print("FOO", s[-1:], err)
+
+        if err is UNTERMINATED:
+            s = s + '"'
+        elif err is EXPECTING_PROPERTY and s[-1:].isalnum():
+            s = s + '"}'
+        elif err is EXPECTING_PROPERTY and s[-1:] == ',':
+            print('BAR')
+            s = s[:-1]
+        elif err is OOB and s[-2:] == ',"':
+            s = s[:-2]
+        elif err is OOB and s[-1:] == '"':
+            s = s[:-1]
+        elif err is OOB and s[-1:] == ':':
+            s = s + '""'
+        elif err is EXPECTING_COLON:
+            s = s + ':""'
         elif find_open_bracket(s):
-            log.debug('FOOO')
             c = find_open_bracket(s)
             if c:
                 s = s + c
-        elif err == EXPECTING_OBJECT:
+        elif err is EXPECTING_OBJECT:
             if s[-1:] == ',':
                 s = s[:-1]
             else:
                 s = s + '}'
-        elif err == EXPECTING_COLON:
-            s = s + ':""'
-        elif err == EXPECTING_PROPERTY and s[-1:].isalnum():
-            s = s + '"}'
-        elif err == OOB and s[-2:] == ',"':
-            s = s[:-2]
-        elif err == OOB and s[-1:] == '"':
-            s = s[:-1]
-        elif err == OOB and s[-1:] == ':':
-            s = s + '""'
         elif err is EXPECTING_COMMA and last_error is EXPECTING_COMMA:
-            s = s[:-4]
+            s = s + '}'
+        elif err is EXPECTING_COMMA and last_error is EXPECTING_PROPERTY:
+            s = s + '}'
+        elif err is EXPECTING_COMMA and last_error is UNTERMINATED:
+            s = s + '}'
         elif err is EXPECTING_COMMA:
             s = s + ','
         elif err is EXPECTING_VALUE:
@@ -71,55 +77,50 @@ def parse_truncated_json(s, depth=0, last_error=None):
 
 
 def find_open_bracket(s):
-    log.debug("find: {}".format(s))
     lsb = s.rfind('[')
     rsb = s.rfind(']')
     if lsb > -1 and (rsb == -1 or lsb > rsb):
         sb_append = ']'
     else:
         sb_append = None
-    log.debug('sb_append: {}'.format(sb_append))
 
     lbr = s.rfind('{')
     rbr = s.rfind('}')
-    log.debug('lbr: {}, rbr {}'.format(lbr, rbr))
     if lbr > -1 and (rbr == -1 or lbr > rbr):
         br_append = '}'
     else:
         br_append = None
-    log.debug('br_append: {}'.format(br_append))
 
-    log.debug('lbr: {}, lsb: {}'.format(lbr, lsb))
     if sb_append and not br_append:
         return sb_append
     elif br_append and not sb_append:
         return br_append
     elif lbr > lsb:
-        log.debug('br')
         return br_append
     else:
-        log.debug('sb')
         return sb_append
 
 
 def parse_error(msg):
+    c = int(re.findall(r'\(char (\d+)\)', str(msg))[0])
+
     if UNTERMINATED in msg:
-        return UNTERMINATED
+        return UNTERMINATED, c
     if EXPECTING_OBJECT in msg:
-        return EXPECTING_OBJECT
+        return EXPECTING_OBJECT, c
     if EXPECTING_COLON in msg:
-        return EXPECTING_COLON
+        return EXPECTING_COLON, c
     if EXPECTING_PROPERTY in msg:
-        return EXPECTING_PROPERTY
+        return EXPECTING_PROPERTY, c
     if OOB in msg:
-        return OOB
+        return OOB, c
     if EXPECTING_COMMA in msg:
-        return EXPECTING_COMMA
+        return EXPECTING_COMMA, c
     if NO_JSON in msg:
-        return NO_JSON
+        return NO_JSON, c
     if EXPECTING_VALUE in msg:
-        return EXPECTING_VALUE
-    return None
+        return EXPECTING_VALUE, c
+    return None, c
 
 
 def find_field(s, name='source'):
@@ -167,6 +168,8 @@ if __name__ == "__main__":
     failed = 0
     succeeded = 0
     for line in sys.stdin.readlines():
+        log.debug('-'*100)
+        log.debug(line)
         out = {}
         out['took'] = find_field(line, name='took')
         out['level'] = find_level(line)
@@ -182,7 +185,7 @@ if __name__ == "__main__":
             o = parse_truncated_json(source)
         except Exception as e:
             failed += 1
-            log.warn("Failed to parse line; Error: {}; line: {}".format(
+            log.warning("Failed to parse line; Error: {}; line: {}".format(
                 e.args[0], line))
             print(line)
             continue
@@ -190,7 +193,7 @@ if __name__ == "__main__":
         succeeded += 1
 
         f.write(json.dumps(out))
-        # log.info(out)
+        log.info(out)
     f.close()
 
     log.info("S: {}; F: {}".format(succeeded, failed))
