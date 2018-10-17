847cce2033b6e75563c2f5bb9e92df91ae50fab4
Alex Forbes <alex.forbes@starlingbank.com>
2018-10-17 13:11:32 +0100

    wip

--- parse_awses_slowlogs.py
+++ parse_awses_slowlogs.py
@@ -6,16 +6,21 @@ EXPECTING_OBJECT = "Expecting object"
 EXPECTING_COLON = "Expecting : delimiter"
 EXPECTING_PROPERTY = "Expecting property name:"
 OOB = "end is out of bounds"
+EXPECTING_COMMA = "Expecting , delimiter"
+NO_JSON = "No JSON object could be decoded"
 
 
 def parse_truncated_json(s, depth=0, last_error=None):
-    if depth > 3:
-        raise Exception("Too deep, last error was: {}".format(last_error))
+    print(s)
+    if depth > 10:
+        raise Exception("Too deep for string {}, last error: {}".format(
+            s, last_error))
     try:
         o = json.loads(s)
     except ValueError as e:
         # print('string {} gives {}'.format(s, e.args[0]))
         err = parse_error(e.args[0])
+        print('err: {}'.format(err))
 
         depth += 1
         if err == UNTERMINATED:
@@ -23,6 +28,10 @@ def parse_truncated_json(s, depth=0, last_error=None):
                 s = s + '}'
             else:
                 s = s + '"'
+        elif find_open_bracket(s):
+            c = find_open_bracket(s)
+            if c:
+                s = s + c
         elif err == EXPECTING_OBJECT:
             if s[-1:] == ',':
                 s = s[:-1]
@@ -31,15 +40,45 @@ def parse_truncated_json(s, depth=0, last_error=None):
         elif err == EXPECTING_COLON:
             s = s + ':""'
         elif err == EXPECTING_PROPERTY and s[-1:].isalnum():
-            # s = s + ':""'
             s = s + '"}'
         elif err == OOB and s[-2:] == ',"':
             s = s[:-2]
+        elif err == OOB and s[-1:] == '"':
+            s = s[:-1]
+        elif err == OOB and s[-1:] == ':':
+            s = s + '""'
+        elif err == EXPECTING_COMMA:
+            c = find_open_bracket(s)
+            if c:
+                s = s + c
+        elif err == NO_JSON:
+            s = s[:-1]
         else:
-            raise Exception("Don't know how to deal with this: {}".format(
-                e.args[0]))
+            raise Exception("Failed to parse string '{}': {}".format(
+                s, e.args[0]))
         return parse_truncated_json(s, depth, last_error=OOB)
     return o
+
+
+def find_open_bracket(s):
+    lsb = s.rfind('[')
+    rsb = s.rfind(']')
+    if lsb and (not rsb or lsb > rsb):
+        sb_append = ']'
+    else:
+        sb_append = None
+
+    lbr = s.rfind('{')
+    rbr = s.rfind('}')
+    if lbr and (not rbr or lbr > rbr):
+        br_append = '}'
+    else:
+        br_append = None
+
+    if lbr > lsb:
+        return br_append
+    else:
+        return sb_append
 
 
 def parse_error(msg):
@@ -53,6 +92,10 @@ def parse_error(msg):
         return EXPECTING_PROPERTY
     if OOB in msg:
         return OOB
+    if EXPECTING_COMMA in msg:
+        return EXPECTING_COMMA
+    if NO_JSON in msg:
+        return NO_JSON
     return None
 
 
@@ -90,7 +133,9 @@ for line in sys.stdin.readlines():
     except Exception as e:
         failed += 1
         print("Failed to parse line; Error: {}; line: {}".format(
-            e.args[0], source))
+            e.args[0], line))
         continue
     succeeded += 1
     # print(o)
+
+print("S: {}; F: {}".format(succeeded, failed))
